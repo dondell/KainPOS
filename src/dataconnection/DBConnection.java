eb98ff7be67f63a73cc265f6d503011fdb19685a
dondell <dondellbatac@gmail.com>
2017-03-24 12:12:59 +0800

    Revert "Update mysql credentials"
    This reverts commit 8ed38a6ecf8bf5060e4b2ea7b76f01e53ea3485c.

--- src/dataconnection/DBConnection.java
+++ src/dataconnection/DBConnection.java
@@ -16,8 +16,8 @@ public class DBConnection {
     private static String readFile = System.getProperty("user.dir") + "/src/dataconnection/config.ini";
     private static String jdbcUrl = "jdbc:mysql://";
     private static String dbUrl;
-    private static String mySqlUsername = "admin";   //Default username value
-    private static String mySqlPassword = "admin";  //Default password value
+    private static String mySqlUsername = "root";   //Default username value
+    private static String mySqlPassword = "*41Ai2841~";  //Default password value
     public static Connection conn;
 
     /**
